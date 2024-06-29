02a943fa292f9fc9661c8df8d81ac33f35e4f88b
PeterChoi <soww12345@gmail.com>
2024-06-29 20:04:39 +0900

    Feat: API 요청 샘플코드 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { apiCall } from '../src/utils/apiCall'
+import { API_LIST } from '../src/utils/apiList'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [testData, setTestData] = useState([])
+  const [greeting, setGreeting] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+    fetchTestData()    
+  }, [])
+
+  const fetchTestData = async () => {
+    setLoading(true)
+      const response = await apiCall(API_LIST.TEST_MULTIPLE_DATA)
+      if (response.status) {
+        setTestData(response.data)
+        setLoading(false)
+      } else {
+        alert('에러 발생')
+        setLoading(false)
+      }            
+  }
+
+  const fetchGreeting = async () => {
+    if (!name) {
+      alert('이름을 입력해주세요.')
+      return
+    }
+
+    const parameters = {
+      name: name,
+    }
+    const response = await apiCall(API_LIST.TEST_GREETING, parameters)
+    if (response.status) {
+      setGreeting(response.data);
+    }    
+  }
+
+  const handleNameChange = (e) => {
+    setName(e.target.value)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
+        오른쪽 버튼을 눌러보세요 &nbsp;
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <h2>테스트 API 호출해보기</h2>
+        {loading ? (
+          <p>테스트 데이터를 가져오고 있습니다...</p>
+        ) : testData.length > 0 ? (
+          <div>
+            {testData.map((item) => (
+              <div key={item.id} style={{margin: '10px 0'}}>
+                <span style={{marginRight: '10px'}}>ID: {item.id}</span>
+                <span>이름: {item.name}</span>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>데이터를 불러오지 못했습니다.</p>
+        )}
+        <button onClick={fetchTestData}>테스트 데이터 가져오기</button>
+      </div>
+
+      <br></br>
+
+      <div>
+        <h2>Greeting 테스트</h2>
+        {greeting ? (
+          <div style={{margin: '10px 0'}}>
+            <span>환영합니다 {greeting.name}님!</span>
+          </div>
+        ) : (
+          <div>
+            <input 
+              type="text" 
+              placeholder="당신의 이름은?"
+              value={name}
+              onChange={handleNameChange}
+            />              
+          </div>
+        )}
+        <button onClick={fetchGreeting}>서버로 나의 이름 전송하기</button>
+      </div>
+
     </>
   )
 }
