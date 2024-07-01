126eb00eef93ba176a6af6c4c0b748fb13d4d700
doyeon kim <kim2004on@gmail.com>
2024-07-02 00:20:32 +0900

    Feat: API 서비스 구현

---
+++ src/components/services/apiService.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const apiService = axios.create({
+  baseURL: 'http://localhost:5000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+apiService.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default apiService;
