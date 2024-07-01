6a0080e93fa23378391f5931184b6e2834105c46
doyeon kim <kim2004on@gmail.com>
2024-07-02 00:21:07 +0900

    Fix: vite.config 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'process.env': {
+      VITE_OPENVIDU_URL: process.env.VITE_OPENVIDU_URL || 'http://localhost:4443',
+      VITE_OPENVIDU_SECRET: process.env.VITE_OPENVIDU_SECRET || 'MY_SECRET',
+    }
+  }
+});
