920e73bdae49f06c4af47609d6e7944bef7e0511
PeterChoi <soww12345@gmail.com>
2024-07-09 22:20:17 +0900

    Styles: 모듈 단위로 CSS 적용 및 아바타 설정에 랜덤성 부여

--- src/components/common/AvatarApp.jsx
+++ src/components/common/AvatarApp.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import './AvatarApp.css';
+import styles from './AvatarApp.module.css';
 import { Canvas, useFrame, useGraph } from '@react-three/fiber';
 import { Color, Euler, Matrix4, SkinnedMesh } from 'three';
 import { useGLTF } from '@react-three/drei';
@@ -15,100 +15,145 @@ let blendshapes = [];
 
 /* App 컴포넌트 */
 function AvatarApp() {
-  const [url, setUrl] = useState("https://models.readyplayer.me/6682c315649e11cdd6dd8a8a.glb");
-  const handleOnChange = (event) => {
-    setUrl(event.target.value);
-  };
+    const randomUrl = [
+        'https://models.readyplayer.me/6682c315649e11cdd6dd8a8a.glb',
+        'https://models.readyplayer.me/66857649a6014cc4b10e8f73.glb',
+    ];
+    const num = Math.floor(Math.random() * 2);
 
-  /* 비디오 설정 */
-  const setup = async () => {
-    const vision = await FilesetResolver.forVisionTasks(
-      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+    const [url, setUrl] = useState(
+        // 'https://models.readyplayer.me/6682c315649e11cdd6dd8a8a.glb'
+        // 'https://models.readyplayer.me/66857649a6014cc4b10e8f73.glb'
+        randomUrl[num]
     );
-    faceLandmarker = await FaceLandmarker.createFromOptions(
-      vision,
-      {
-        baseOptions: {
-          modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
-          delegate: "GPU"
-        },
-        outputFaceBlendshapes: true,
-        outputFacialTransformationMatrixes: true,
-        runningMode: "VIDEO"
-      });
-    video = document.getElementById('video');
-    navigator.mediaDevices.getUserMedia({
-      video: { width: 1280, height: 720 }
-    }).then((stream) => {
-      video.srcObject = stream;
-      video.addEventListener('loadeddata', predict);
-    });
-  };
+    const handleOnChange = (event) => {
+        setUrl(event.target.value);
+    };
 
-  const predict = () => {
-    let startTimeMs = performance.now();
-    if (lastVideoTime !== video.currentTime) {
-      lastVideoTime = video.currentTime;
-      const result = faceLandmarker.detectForVideo(video, startTimeMs);
-      if (result.facialTransformationMatrixes && result.facialTransformationMatrixes.length > 0 && result.faceBlendshapes && result.faceBlendshapes.length > 0) {
-        const matrix = new Matrix4().fromArray(result.facialTransformationMatrixes[0].data);
-        rotation = new Euler().setFromRotationMatrix(matrix);
+    /* 비디오 설정 */
+    const setup = async () => {
+        const vision = await FilesetResolver.forVisionTasks(
+            'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm'
+        );
+        faceLandmarker = await FaceLandmarker.createFromOptions(vision, {
+            baseOptions: {
+                modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+                delegate: 'GPU',
+            },
+            outputFaceBlendshapes: true,
+            outputFacialTransformationMatrixes: true,
+            runningMode: 'VIDEO',
+        });
+        video = document.getElementById('video');
+        navigator.mediaDevices
+            .getUserMedia({
+                video: { width: 1280, height: 720 },
+            })
+            .then((stream) => {
+                video.srcObject = stream;
+                video.addEventListener('loadeddata', predict);
+            });
+    };
 
-        blendshapes = result.faceBlendshapes[0].categories;
-      }
-    }
-    requestAnimationFrame(predict);
-  };
+    const predict = () => {
+        let startTimeMs = performance.now();
+        if (lastVideoTime !== video.currentTime) {
+            lastVideoTime = video.currentTime;
+            const result = faceLandmarker.detectForVideo(video, startTimeMs);
+            if (
+                result.facialTransformationMatrixes &&
+                result.facialTransformationMatrixes.length > 0 &&
+                result.faceBlendshapes &&
+                result.faceBlendshapes.length > 0
+            ) {
+                const matrix = new Matrix4().fromArray(
+                    result.facialTransformationMatrixes[0].data
+                );
+                rotation = new Euler().setFromRotationMatrix(matrix);
 
-  useEffect(() => {
-    setup();
-  }, []);
+                blendshapes = result.faceBlendshapes[0].categories;
+            }
+        }
+        requestAnimationFrame(predict);
+    };
 
-  return (
-    <div className="AvatarApp">
-      <input type="text" placeholder='Enter your RPM avatar URL' onChange={handleOnChange} />
-      <video autoPlay id='video' />
-      <Canvas style={{
-        backgroundColor: 'purple',
-        height: 400,
-      }}
-        camera={{
-          fov: 25
-        }}>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[1, 1, 1]} color={new Color(1, 0, 0)} intensity={0.5} />
-        <pointLight position={[-1, 0, 1]} color={new Color(0, 1, 0)} intensity={0.5} />
+    useEffect(() => {
+        setup();
+    }, []);
 
-        <Avatar url={url} />
-      </Canvas>
-    </div>
-  );
+    return (
+        <div
+            className={styles.AvatarApp}
+            style={{
+                position: 'absolute',
+                left: '-9999px',
+                top: '-9999px',
+            }}
+        >
+            <input
+                className={styles.url_input}
+                type="text"
+                placeholder="Enter your RPM avatar URL"
+                onChange={handleOnChange}
+            />
+            <video className={styles.avartar_video} autoPlay id="video" />
+            <Canvas
+                id="avatar_canvas"
+                style={{
+                    backgroundColor: 'purple',
+                    height: 400,
+                }}
+                camera={{
+                    fov: 25,
+                }}
+            >
+                <ambientLight intensity={0.5} />
+                <pointLight
+                    position={[1, 1, 1]}
+                    color={new Color(1, 0, 0)}
+                    intensity={0.5}
+                />
+                <pointLight
+                    position={[-1, 0, 1]}
+                    color={new Color(0, 1, 0)}
+                    intensity={0.5}
+                />
+
+                <Avatar url={url} />
+            </Canvas>
+        </div>
+    );
 }
 
 /* 아바타 컴포넌트 */
 function Avatar({ url }) {
-  const avatar = useGLTF(`${url}?morphTargets=ARKit&textureAtlas=1024`);
-  const { nodes } = useGraph(avatar.scene);
-  useEffect(() => {
-    headMesh = nodes.Wolf3D_Avatar;
-  }, [nodes]);
+    const avatar = useGLTF(`${url}?morphTargets=ARKit&textureAtlas=1024`);
+    const { nodes } = useGraph(avatar.scene);
+    useEffect(() => {
+        headMesh = nodes.Wolf3D_Avatar;
+    }, [nodes]);
 
-  useFrame(() => {
-    if (headMesh !== null) {
-      blendshapes.forEach((blendshape) => {
-        let index = headMesh.morphTargetDictionary[blendshape.categoryName];
-        if (index >= 0) {
-          headMesh.morphTargetInfluences[index] = blendshape.score;
+    useFrame(() => {
+        if (headMesh !== null) {
+            blendshapes.forEach((blendshape) => {
+                let index =
+                    headMesh.morphTargetDictionary[blendshape.categoryName];
+                if (index >= 0) {
+                    headMesh.morphTargetInfluences[index] = blendshape.score;
+                }
+            });
         }
-      });
-    }
 
-    nodes.Head.rotation.set(rotation.x / 3, rotation.y / 3, rotation.z / 3);
-    nodes.Neck.rotation.set(rotation.x / 3, rotation.y / 3, rotation.z / 3);
-    nodes.Spine1.rotation.set(rotation.x / 3, rotation.y / 3, rotation.z / 3);
-  });
+        nodes.Head.rotation.set(rotation.x / 3, rotation.y / 3, rotation.z / 3);
+        nodes.Neck.rotation.set(rotation.x / 3, rotation.y / 3, rotation.z / 3);
+        nodes.Spine1.rotation.set(
+            rotation.x / 3,
+            rotation.y / 3,
+            rotation.z / 3
+        );
+    });
 
-  return <primitive object={avatar.scene} position={[0, -1.65, 4]} />;
+    return <primitive object={avatar.scene} position={[0, -1.65, 4]} />;
 }
 
 export default AvatarApp;
