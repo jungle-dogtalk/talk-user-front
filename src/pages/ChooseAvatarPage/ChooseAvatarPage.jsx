0b2f83d2f2aed90ff936b94112dfb30c88ef7c0d
PeterChoi <soww12345@gmail.com>
2024-07-12 01:37:55 +0900

    Styles: es lint 적용

--- src/pages/ChooseAvatarPage/ChooseAvatarPage.jsx
+++ src/pages/ChooseAvatarPage/ChooseAvatarPage.jsx
@@ -16,7 +16,9 @@ let blendshapes = [];
 
 const ChooseAvatarPage = () => {
     const webcamRef = useRef(null);
-    const [url, setUrl] = useState("https://models.readyplayer.me/6682c315649e11cdd6dd8a8a.glb");
+    const [url, setUrl] = useState(
+        'https://models.readyplayer.me/6682c315649e11cdd6dd8a8a.glb'
+    );
     const navigate = useNavigate(); // useNavigate 훅 사용
 
     const handleOnChange = (event) => {
@@ -25,45 +27,56 @@ const ChooseAvatarPage = () => {
 
     useEffect(() => {
         // 웹캠 스트림 설정
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then(stream => {
+        navigator.mediaDevices
+            .getUserMedia({ video: true })
+            .then((stream) => {
                 webcamRef.current.srcObject = stream;
             })
-            .catch(error => {
-                console.error("Error accessing webcam:", error);
+            .catch((error) => {
+                console.error('Error accessing webcam:', error);
             });
 
         const setup = async () => {
             const vision = await FilesetResolver.forVisionTasks(
-                "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+                'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm'
             );
-            faceLandmarker = await FaceLandmarker.createFromOptions(
-                vision,
-                {
-                    baseOptions: {
-                        modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
-                        delegate: "GPU"
-                    },
-                    outputFaceBlendshapes: true,
-                    outputFacialTransformationMatrixes: true,
-                    runningMode: "VIDEO"
-                });
-            video = document.getElementById('video');
-            navigator.mediaDevices.getUserMedia({
-                video: { width: 1280, height: 720 }
-            }).then((stream) => {
-                video.srcObject = stream;
-                video.addEventListener('loadeddata', predict);
+            faceLandmarker = await FaceLandmarker.createFromOptions(vision, {
+                baseOptions: {
+                    modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+                    delegate: 'GPU',
+                },
+                outputFaceBlendshapes: true,
+                outputFacialTransformationMatrixes: true,
+                runningMode: 'VIDEO',
             });
+            video = document.getElementById('video');
+            navigator.mediaDevices
+                .getUserMedia({
+                    video: { width: 1280, height: 720 },
+                })
+                .then((stream) => {
+                    video.srcObject = stream;
+                    video.addEventListener('loadeddata', predict);
+                });
         };
 
         const predict = () => {
             let startTimeMs = performance.now();
             if (lastVideoTime !== video.currentTime) {
                 lastVideoTime = video.currentTime;
-                const result = faceLandmarker.detectForVideo(video, startTimeMs);
-                if (result.facialTransformationMatrixes && result.facialTransformationMatrixes.length > 0 && result.faceBlendshapes && result.faceBlendshapes.length > 0) {
-                    const matrix = new Matrix4().fromArray(result.facialTransformationMatrixes[0].data);
+                const result = faceLandmarker.detectForVideo(
+                    video,
+                    startTimeMs
+                );
+                if (
+                    result.facialTransformationMatrixes &&
+                    result.facialTransformationMatrixes.length > 0 &&
+                    result.faceBlendshapes &&
+                    result.faceBlendshapes.length > 0
+                ) {
+                    const matrix = new Matrix4().fromArray(
+                        result.facialTransformationMatrixes[0].data
+                    );
                     rotation = new Euler().setFromRotationMatrix(matrix);
 
                     blendshapes = result.faceBlendshapes[0].categories;
@@ -79,16 +92,16 @@ const ChooseAvatarPage = () => {
         // 아바타 변경 로직 (임의의 아바타 이미지 설정)
 
         const urls = [
-            "https://models.readyplayer.me/668293698a5ecca99f9b06c1.glb",
-            "https://models.readyplayer.me/6682c315649e11cdd6dd8a8a.glb",
-            "https://models.readyplayer.me/66857649a6014cc4b10e8f73.glb",
-            "https://models.readyplayer.me/668d1456878f8e58dc12d758.glb",
-            "https://models.readyplayer.me/668d14b51847c40762af418e.glb",
-            "https://models.readyplayer.me/668d14d83369b0756b9c487c.glb",
-            "https://models.readyplayer.me/668d14e93369b0756b9c48b0.glb",
-            "https://models.readyplayer.me/668d14f634432ca7edca24af.glb",
-            "https://models.readyplayer.me/668d150a7a0772243cddc4af.glb",
-            "https://models.readyplayer.me/668d152063703fb7530e8a0d.glb"
+            'https://models.readyplayer.me/668293698a5ecca99f9b06c1.glb',
+            'https://models.readyplayer.me/6682c315649e11cdd6dd8a8a.glb',
+            'https://models.readyplayer.me/66857649a6014cc4b10e8f73.glb',
+            'https://models.readyplayer.me/668d1456878f8e58dc12d758.glb',
+            'https://models.readyplayer.me/668d14b51847c40762af418e.glb',
+            'https://models.readyplayer.me/668d14d83369b0756b9c487c.glb',
+            'https://models.readyplayer.me/668d14e93369b0756b9c48b0.glb',
+            'https://models.readyplayer.me/668d14f634432ca7edca24af.glb',
+            'https://models.readyplayer.me/668d150a7a0772243cddc4af.glb',
+            'https://models.readyplayer.me/668d152063703fb7530e8a0d.glb',
         ];
         const randomUrl = urls[Math.floor(Math.random() * urls.length)];
         setUrl(randomUrl);
@@ -96,7 +109,7 @@ const ChooseAvatarPage = () => {
 
     const handleSelect = () => {
         // 아바타 선택 후 처리 로직
-        console.log("Selected avatar url:", url);
+        console.log('Selected avatar url:', url);
         navigate('/videochat');
     };
 
@@ -108,26 +121,50 @@ const ChooseAvatarPage = () => {
             <div className="content">
                 <div className="section">
                     <h2>나의 모습</h2>
-                    <video ref={webcamRef} autoPlay id='video' className="video-box"></video>
+                    <video
+                        ref={webcamRef}
+                        autoPlay
+                        id="video"
+                        className="video-box"
+                    ></video>
                 </div>
                 <div className="section">
                     <h2>내 아바타</h2>
-                    <Canvas className="avatar-box"
+                    <Canvas
+                        className="avatar-box"
                         camera={{
-                            fov: 25
-                        }}>
+                            fov: 25,
+                        }}
+                    >
                         <ambientLight intensity={0.5} />
-                        <pointLight position={[1, 1, 1]} color={new Color(1, 0, 0)} intensity={0.5} />
-                        <pointLight position={[-1, 0, 1]} color={new Color(0, 1, 0)} intensity={0.5} />
+                        <pointLight
+                            position={[1, 1, 1]}
+                            color={new Color(1, 0, 0)}
+                            intensity={0.5}
+                        />
+                        <pointLight
+                            position={[-1, 0, 1]}
+                            color={new Color(0, 1, 0)}
+                            intensity={0.5}
+                        />
 
                         <Avatar url={url} />
                     </Canvas>
                 </div>
             </div>
             <div className="buttons">
-                <button className="action-button" onClick={() => window.history.back()}>뒤로가기</button>
-                <button className="action-button" onClick={handleRetry}>다시 추천</button>
-                <button className="action-button" onClick={handleSelect}>선택</button>
+                <button
+                    className="action-button"
+                    onClick={() => window.history.back()}
+                >
+                    뒤로가기
+                </button>
+                <button className="action-button" onClick={handleRetry}>
+                    다시 추천
+                </button>
+                <button className="action-button" onClick={handleSelect}>
+                    선택
+                </button>
             </div>
         </div>
     );
@@ -143,7 +180,8 @@ function Avatar({ url }) {
     useFrame(() => {
         if (headMesh !== null) {
             blendshapes.forEach((blendshape) => {
-                let index = headMesh.morphTargetDictionary[blendshape.categoryName];
+                let index =
+                    headMesh.morphTargetDictionary[blendshape.categoryName];
                 if (index >= 0) {
                     headMesh.morphTargetInfluences[index] = blendshape.score;
                 }
@@ -152,7 +190,11 @@ function Avatar({ url }) {
 
         nodes.Head.rotation.set(rotation.x / 3, rotation.y / 3, rotation.z / 3);
         nodes.Neck.rotation.set(rotation.x / 3, rotation.y / 3, rotation.z / 3);
-        nodes.Spine1.rotation.set(rotation.x / 3, rotation.y / 3, rotation.z / 3);
+        nodes.Spine1.rotation.set(
+            rotation.x / 3,
+            rotation.y / 3,
+            rotation.z / 3
+        );
     });
 
     return <primitive object={avatar.scene} position={[0, -1.65, 4]} />;
