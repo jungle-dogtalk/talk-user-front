564aa3f0954997a146ee579b1fb205554d937e05
PeterChoi <soww12345@gmail.com>
2024-07-13 23:25:03 +0900

    Refactor: 조건문에 중괄호 추가

--- src/pages/VideoChatPage/OpenViduVideo.jsx
+++ src/pages/VideoChatPage/OpenViduVideo.jsx
@@ -4,8 +4,9 @@ const OpenViduVideo = (props) => {
     const videoRef = useRef();
 
     useEffect(() => {
-        if (props.streamManager && videoRef)
+        if (props.streamManager && videoRef) {
             props.streamManager.addVideoElement(videoRef.current);
+        }
     }, [props.streamManager]);
 
     return <video autoPlay={true} ref={videoRef} />;
