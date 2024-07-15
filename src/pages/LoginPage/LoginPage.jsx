147d275d6f0b03d03241f49f4e1c73b54b63a26e
doyeon kim <kim2004on@gmail.com>
2024-07-15 17:11:57 +0900

    Design: 로그인 페이지 반응형(iphone 12 pro) 적용

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -31,18 +31,22 @@ const LoginPage = () => {
 
     return (
         <div className="min-h-screen flex flex-col bg-[#f7f3e9]">
-            <div className="flex flex-col justify-center items-center flex-1 px-16">
-                <img src={logo} alt="명톡 로고" className="w-96 mb-16" />
+            <div className="flex flex-col justify-center items-center flex-1 px-4 sm:px-16">
+                <img
+                    src={logo}
+                    alt="명톡 로고"
+                    className="w-36 sm:w-96 mb-8 sm:mb-16"
+                />
                 <form
                     onSubmit={handleLogin}
-                    className="space-y-8 w-full max-w-2xl flex flex-col items-center"
+                    className="space-y-10 w-full max-w-xs sm:max-w-2xl flex flex-col items-center"
                 >
-                    <div className="space-y-4 w-full flex justify-center items-center">
-                        <div className="flex flex-col space-y-4 w-3/4">
-                            <div className="flex items-center space-x-4">
+                    <div className="space-y-2 w-full flex flex-col sm:flex-row justify-center items-center">
+                        <div className="flex flex-col space-y-6 w-full sm:w-3/4">
+                            <div className="flex flex-col sm:flex-row items-center space-x-0 sm:space-x-4">
                                 <label
                                     htmlFor="username"
-                                    className="block text-2xl font-medium text-gray-700 w-20"
+                                    className="block text-xl sm:text-2xl font-medium text-gray-700 w-full sm:w-20"
                                 >
                                     ID
                                 </label>
@@ -55,13 +59,13 @@ const LoginPage = () => {
                                     }
                                     placeholder="아이디를 입력하세요"
                                     required
-                                    className="flex-1 block px-6 py-4 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 text-2xl w-full"
+                                    className="block px-4 py-2 sm:px-6 sm:py-4 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 text-xl sm:text-2xl w-full"
                                 />
                             </div>
-                            <div className="flex items-center space-x-4">
+                            <div className="flex flex-col sm:flex-row items-center space-x-0 sm:space-x-4">
                                 <label
                                     htmlFor="password"
-                                    className="block text-2xl font-medium text-gray-700 w-20"
+                                    className="block text-xl sm:text-2xl font-medium text-gray-700 w-full sm:w-20"
                                 >
                                     PW
                                 </label>
@@ -74,39 +78,41 @@ const LoginPage = () => {
                                     }
                                     placeholder="비밀번호를 입력하세요"
                                     required
-                                    className="flex-1 block px-6 py-4 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 text-2xl w-full"
+                                    className="block px-4 py-2 sm:px-6 sm:py-4 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 text-xl sm:text-2xl w-full"
                                 />
                             </div>
                         </div>
-                        <button
-                            type="submit"
-                            className="ml-6 py-6 px-8 bg-[#a16e47] text-white rounded-full border-2 border-[#a16e47] shadow-md hover:bg-[#8f5f38] hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#8f5f38] text-3xl w-35 text-center whitespace-nowrap"
-                        >
-                            로그인
-                        </button>
+                        <div className="w-full sm:w-1/4 sm:pl-4">
+                            <button
+                                type="submit"
+                                className="mt-8 sm:mt-0 py-4 sm:py-6 px-6 sm:px-8 bg-[#a16e47] text-white rounded-full border-2 border-[#a16e47] shadow-md hover:bg-[#8f5f38] hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#8f5f38] text-xl sm:text-3xl w-full text-center whitespace-nowrap"
+                            >
+                                로그인
+                            </button>
+                        </div>
                     </div>
                 </form>
                 {loading && (
-                    <p className="mt-8 text-center text-gray-500 text-3xl">
+                    <p className="mt-8 text-center text-gray-500 text-xl sm:text-3xl">
                         Loading...
                     </p>
                 )}
                 {error && (
-                    <p className="mt-8 text-center text-red-500 text-3xl">
+                    <p className="mt-8 text-center text-red-500 text-xl sm:text-3xl">
                         {error}
                     </p>
                 )}
-                <div className="mt-10 text-center">
+                <div className="mt-10 text-center text-lg sm:text-3xl">
                     <a
                         href="/signup"
-                        className="text-3xl text-gray-600 hover:underline transition duration-300 ease-in-out transform hover:scale-105"
+                        className="text-gray-600 hover:underline transition duration-300 ease-in-out transform hover:scale-105"
                     >
                         회원가입 하기
                     </a>
-                    <span className="mx-6">|</span>
+                    <span className="mx-4 sm:mx-6">|</span>
                     <a
                         href="/forgot-password"
-                        className="text-3xl text-gray-600 hover:underline transition duration-300 ease-in-out transform hover:scale-105"
+                        className="text-gray-600 hover:underline transition duration-300 ease-in-out transform hover:scale-105"
                     >
                         ID/PW 찾기
                     </a>
