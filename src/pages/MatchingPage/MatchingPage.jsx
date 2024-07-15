290409493a3de34e43d0464b4c580bb4cf9ce3bf
doyeon kim <kim2004on@gmail.com>
2024-07-15 23:52:18 +0900

    Design: 매칭 페이지 UI 수정 - (Tailwind CSS) 적용

--- src/pages/MatchingPage/MatchingPage.jsx
+++ src/pages/MatchingPage/MatchingPage.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import './MatchingPage.css';
+import { useNavigate } from 'react-router-dom';
 import waitingDogImage from '../../assets/dog.png'; // 강아지 이미지
 import waitingHouseImage from '../../assets/doghouse.jpg'; // 강아지 집 이미지
 import { io } from 'socket.io-client';
 import { apiCall } from '../../utils/apiCall';
 import { API_LIST } from '../../utils/apiList';
+import logo from '../../assets/barking-talk.png';
 
 const MatchingPage = () => {
+    const navigate = useNavigate();
     const userInfo = useSelector((state) => state.user.userInfo);
-    console.log('유저인포 ->  ', userInfo);
-
     const socket = io(import.meta.env.VITE_API_URL);
 
-    //사용자 데이터를 query 아닌 소켓으로 전송하게 수정했음.
+    // 사용자 데이터를 query 아닌 소켓으로 전송하게 수정했음.
     useEffect(() => {
         socket.emit('userDetails', {
             userId: userInfo.username,
@@ -38,12 +38,8 @@ const MatchingPage = () => {
         };
     }, []);
 
-    const handleStopClick = () => {
-        alert('중단하기 버튼이 클릭되었습니다.');
-    };
-
     const handleCancelClick = () => {
-        alert('취소하기 버튼이 클릭되었습니다.');
+        navigate('/main');
     };
 
     // 백엔드 서버 콘솔로그에서 OpenVidu 가용 세션 확인하기 위한 API 호출
@@ -56,48 +52,42 @@ const MatchingPage = () => {
     }, []);
 
     return (
-        <div className="matching-page">
-            <div className="header">
-                <h1>멍톡</h1>
-            </div>
-
-            <div className="content2">
-                <div className="matching-box">
-                    <div className="matching-status">
-                        <h2>매칭 중...</h2>
-                        <p>예상시간: 8분</p>
-                        <p>{userInfo.interests}</p>
-                        {/* 관심사 데이터 체크용*/}
+        <div className="min-h-screen flex flex-col bg-[#FFFAE8]">
+            <header className="w-full bg-[#a16e47] p-2 flex justify-between items-center">
+                <img src={logo} alt="멍톡 로고" className="w-16 h-16" />
+            </header>
+            <div className="flex flex-col items-center justify-center flex-1 w-full px-4 sm:px-8">
+                <div className="bg-[#FFFAE8] rounded-lg p-8 w-full max-w-5xl flex flex-col items-center">
+                    <div className="text-center">
+                        <h2 className="text-3xl sm:text-5xl font-bold">
+                            매칭 중
+                        </h2>
+                        <p className="text-gray-700 mt-6 text-lg sm:text-2xl">
+                            나의 관심사 : {userInfo.interests.join(', ')}
+                        </p>
                     </div>
-                    <p className="instruction">
-                        화면 아래 집 아이콘을 누르면 통화를 중단할 수 있어요!
-                    </p>
-                    <div className="images">
+                    <div className="flex flex-col sm:flex-row items-center justify-center space-y-8 sm:space-y-0 sm:space-x-16 mt-12">
                         <img
                             src={waitingDogImage}
                             alt="Waiting Dog"
-                            className="waiting-dog"
+                            className="w-48 h-48 sm:w-64 sm:h-64"
                         />
-                        <div className="house-wrapper">
+                        <div className="relative">
                             <img
                                 src={waitingHouseImage}
                                 alt="Waiting House"
-                                className="waiting-house"
+                                className="w-48 h-48 sm:w-64 sm:h-64"
                             />
-                            <button
-                                className="stop-button"
-                                onClick={handleStopClick}
-                            >
-                                중단하기
-                            </button>
                         </div>
                     </div>
-                    <button
-                        className="cancel-button"
-                        onClick={handleCancelClick}
-                    >
-                        취소하기
-                    </button>
+                    <div className="flex justify-center mt-16">
+                        <button
+                            className="bg-[#f7f3e9] text-[#a16e47] py-1 px-3 sm:py-2 sm:px-6 rounded-full border-2 border-[#a16e47] shadow-md hover:bg-[#e4d7c7] hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105 font-semibold text-sm sm:text-lg"
+                            onClick={handleCancelClick}
+                        >
+                            취소하기
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
