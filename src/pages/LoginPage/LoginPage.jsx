55d0029bb8576e7ced7461cd016e2ee6d91bdfbb
doyeon kim <kim2004on@gmail.com>
2024-07-15 13:04:48 +0900

    Design: 로그인 페이지 - 각 버튼들 UI 수정

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -9,7 +9,7 @@ const LoginPage = () => {
     const [password, setPassword] = useState(''); // 비밀번호 상태 변수와 설정 함수
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const { token, error,loading  } = useSelector((state) => state.user); // Redux store의 상태를 읽어오는 데 사용.
+    const { token, error, loading } = useSelector((state) => state.user); // Redux store의 상태를 읽어오는 데 사용.
 
     const handleLogin = async (e) => {
         e.preventDefault(); // 폼 제출시 페이지가 새로고침 되는 것 방지
@@ -18,11 +18,9 @@ const LoginPage = () => {
         // resultAction이 성공적으로 완료되었는지
         if (loginUser.fulfilled.match(resultAction)) {
             navigate('/main'); // 로그인 성공 시 메인 페이지로 이동
+        } else {
+            alert('ID/PW 확인해주세요.');
         }
-        else{
-            alert('ID/PW 확인해주세요.')
-        }
-
     };
 
     useEffect(() => {
@@ -35,50 +33,87 @@ const LoginPage = () => {
         <div className="min-h-screen flex flex-col bg-[#f7f3e9]">
             <div className="flex flex-col justify-center items-center flex-1 px-16">
                 <img src={logo} alt="명톡 로고" className="w-96 mb-16" />
-                <form onSubmit={handleLogin} className="space-y-8 w-full max-w-2xl flex flex-col items-center">
+                <form
+                    onSubmit={handleLogin}
+                    className="space-y-8 w-full max-w-2xl flex flex-col items-center"
+                >
                     <div className="space-y-4 w-full flex justify-center items-center">
                         <div className="flex flex-col space-y-4 w-3/4">
                             <div className="flex items-center space-x-4">
-                                <label htmlFor="username" className="block text-2xl font-medium text-gray-700 w-20">ID</label>
+                                <label
+                                    htmlFor="username"
+                                    className="block text-2xl font-medium text-gray-700 w-20"
+                                >
+                                    ID
+                                </label>
                                 <input
                                     type="text"
                                     id="username"
                                     value={username}
-                                    onChange={(e) => setUsername(e.target.value)}
+                                    onChange={(e) =>
+                                        setUsername(e.target.value)
+                                    }
                                     placeholder="아이디를 입력하세요"
                                     required
                                     className="flex-1 block px-6 py-4 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 text-2xl w-full"
                                 />
                             </div>
                             <div className="flex items-center space-x-4">
-                                <label htmlFor="password" className="block text-2xl font-medium text-gray-700 w-20">PW</label>
+                                <label
+                                    htmlFor="password"
+                                    className="block text-2xl font-medium text-gray-700 w-20"
+                                >
+                                    PW
+                                </label>
                                 <input
                                     type="password"
                                     id="password"
                                     value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
+                                    onChange={(e) =>
+                                        setPassword(e.target.value)
+                                    }
                                     placeholder="비밀번호를 입력하세요"
                                     required
                                     className="flex-1 block px-6 py-4 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 text-2xl w-full"
                                 />
                             </div>
                         </div>
-                        <button type="submit" className="ml-6 py-6 px-8 bg-[#a16e47] text-white rounded-md hover:bg-[#8f5f38] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#8f5f38] text-3xl w-35 text-center whitespace-nowrap">
+                        <button
+                            type="submit"
+                            className="ml-6 py-6 px-8 bg-[#a16e47] text-white rounded-full border-2 border-[#a16e47] shadow-md hover:bg-[#8f5f38] hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#8f5f38] text-3xl w-35 text-center whitespace-nowrap"
+                        >
                             로그인
                         </button>
                     </div>
                 </form>
-                {loading && <p className="mt-8 text-center text-gray-500 text-3xl">Loading...</p>}
-                {error && <p className="mt-8 text-center text-red-500 text-3xl">{error}</p>}
+                {loading && (
+                    <p className="mt-8 text-center text-gray-500 text-3xl">
+                        Loading...
+                    </p>
+                )}
+                {error && (
+                    <p className="mt-8 text-center text-red-500 text-3xl">
+                        {error}
+                    </p>
+                )}
                 <div className="mt-10 text-center">
-                    <a href="/signup" className="text-3xl text-gray-600 hover:underline">회원가입 하기</a>
+                    <a
+                        href="/signup"
+                        className="text-3xl text-gray-600 hover:underline transition duration-300 ease-in-out transform hover:scale-105"
+                    >
+                        회원가입 하기
+                    </a>
                     <span className="mx-6">|</span>
-                    <a href="/forgot-password" className="text-3xl text-gray-600 hover:underline">ID/PW 찾기</a>
+                    <a
+                        href="/forgot-password"
+                        className="text-3xl text-gray-600 hover:underline transition duration-300 ease-in-out transform hover:scale-105"
+                    >
+                        ID/PW 찾기
+                    </a>
                 </div>
             </div>
         </div>
     );
 };
-    
-    
-    export default LoginPage;
+
+export default LoginPage;
