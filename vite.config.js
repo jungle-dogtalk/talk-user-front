a552a6d1651141e53b0b2a38679c327b80971c60
심우정 <dnwjd040635@gmail.com>
2024-07-18 07:20:06 +0900

    Chore: proxy 서버 url 환경변수 적용

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    plugins: [react()],
-    define: {
-        'process.env': {},
-    },
-    server: {
-        proxy: {
-            '/api': 'http://localhost:5000', // 백엔드 서버의 URL로 프록시 설정
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    console.log('URL: ', env.VITE_API_URL);
+
+    return {
+        plugins: [react()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: env.VITE_API_URL,
+                },
+            },
         },
-    },
+    };
 });
