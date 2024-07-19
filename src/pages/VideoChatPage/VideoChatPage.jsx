6d120756f7575a08a3ff0dee50d52dcab5aeab54
심우정 <dnwjd040635@gmail.com>
2024-07-19 20:36:33 +0900

    Chore: 주제 줄바꿈 개선

--- src/pages/VideoChatPage/VideoChatPage.jsx
+++ src/pages/VideoChatPage/VideoChatPage.jsx
@@ -179,24 +179,7 @@ const VideoChatPage = () => {
         // 결과 데이터 수신 받아와 변수에 저장 후 상태 업데이트
         socket.current.on('topicRecommendations', (data) => {
             console.log('Received topic recommendations:', data);
-            setRecommendedTopics((prevTopics) => {
-                if (data === '\n') {
-                    return [...prevTopics, ''];
-                } else {
-                    const updatedTopics = [...prevTopics];
-                    if (updatedTopics.length === 0) {
-                        updatedTopics.push(data);
-                    } else {
-                        updatedTopics[updatedTopics.length - 1] += data;
-                    }
-                    return updatedTopics;
-                }
-            });
-            /*------------본래 주제 한 번에 받아오던 코드-------------*/
-            // const topics = Array.isArray(data.data.topics)
-            //     ? data.data.topics
-            //     : [];
-            // setRecommendedTopics(topics);
+            setRecommendedTopics((prevTopics) => [...prevTopics, data.trim()]);
         });
 
         socket.current.on('endOfStream', () => {
@@ -683,6 +666,7 @@ const VideoChatPage = () => {
     };
 
     function boyerMooreSearch(text, pattern) {
+        console.log('대답 내용: ', text);
         // text 공백 전처리
         text = text.trim();
         pattern = pattern.trim();
@@ -708,6 +692,7 @@ const VideoChatPage = () => {
             }
 
             if (j < 0) {
+                console.log('성공');
                 return true;
             } else {
                 const charCode = text.charCodeAt(s + j);
@@ -716,7 +701,7 @@ const VideoChatPage = () => {
                 s += Math.max(1, badCharShift);
             }
         }
-
+        console.log('실패');
         return false;
     }
 
