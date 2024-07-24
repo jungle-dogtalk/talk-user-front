a40950e9ec666aaaf245504290823dc5e0c6e337
joosomi <wnthal1211@gmail.com>
2024-07-24 19:59:51 +0900

    fix: choose-raccon 실시간 영상에 로딩중, 음성 테스트 디폴트 bar

--- src/pages/ChooseRaccoonPage/ChooseRaccoonPage.jsx
+++ src/pages/ChooseRaccoonPage/ChooseRaccoonPage.jsx
@@ -1,112 +1,128 @@
 import React, { useEffect, useRef, useState } from 'react';
-import logo from '../../assets/barking-talk.png'; // 로고 이미지 경로
+import logo from '../../assets/barking-talk.png';
 import { Canvas, useFrame, useGraph } from '@react-three/fiber';
 import { Color, Euler, Matrix4 } from 'three';
 import { useGLTF } from '@react-three/drei';
 import { FaceLandmarker, FilesetResolver } from '@mediapipe/tasks-vision';
 import { useNavigate } from 'react-router-dom';
 import ChooseRaccoonHand from './ChooseRaccoonHand.jsx';
+import './ChooseRaccoonPage.css';
 
-let video;
 let faceLandmarker;
 let lastVideoTime = -1;
-let headMesh;
 let rotation;
 let blendshapes = [];
 
 const ChooseRaccoonPage = () => {
     const webcamRef = useRef(null);
-    const navigate = useNavigate(); // useNavigate 훅 사용
-    const [videoLoaded, setVideoLoaded] = useState(false); // 비디오 로딩 상태
+    const videoContainerRef = useRef(null);
+    const navigate = useNavigate();
+    const [videoReady, setVideoReady] = useState(false);
+    const [isMicReady, setIsMicReady] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [videoLoaded, setVideoLoaded] = useState(false);
 
     useEffect(() => {
-        // 웹캠 스트림 설정
-        navigator.mediaDevices
-            .getUserMedia({ video: true })
-            .then((stream) => {
-                webcamRef.current.srcObject = stream;
-                webcamRef.current.onloadeddata = () => {
-                    setVideoLoaded(true); // 비디오가 로드되면 상태 변경
-                };
-            })
-            .catch((error) => {
-                console.error('Error accessing webcam:', error);
-            });
-
-        const setup = async () => {
-            const vision = await FilesetResolver.forVisionTasks(
-                'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm'
-            );
-            faceLandmarker = await FaceLandmarker.createFromOptions(vision, {
-                baseOptions: {
-                    modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
-                    delegate: 'GPU',
-                },
-                outputFaceBlendshapes: true,
-                outputFacialTransformationMatrixes: true,
-                runningMode: 'VIDEO',
-            });
-            video = document.getElementById('video');
-            navigator.mediaDevices
-                .getUserMedia({
+        const setupVideoAndFaceLandmarker = async () => {
+            setIsLoading(true);
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({
                     video: { width: 1280, height: 720 },
-                })
-                .then((stream) => {
-                    video.srcObject = stream;
-                    video.addEventListener('loadedmetadata', predict);
                 });
-        };
 
-        const predict = () => {
-            let startTimeMs = performance.now();
-            if (lastVideoTime !== video.currentTime) {
-                lastVideoTime = video.currentTime;
-                const result = faceLandmarker.detectForVideo(
-                    video,
-                    startTimeMs
-                );
-                if (
-                    result.facialTransformationMatrixes &&
-                    result.facialTransformationMatrixes.length > 0 &&
-                    result.faceBlendshapes &&
-                    result.faceBlendshapes.length > 0
-                ) {
-                    const matrix = new Matrix4().fromArray(
-                        result.facialTransformationMatrixes[0].data
-                    );
-                    rotation = new Euler().setFromRotationMatrix(matrix);
-
-                    blendshapes = result.faceBlendshapes[0].categories;
+                if (webcamRef.current) {
+                    webcamRef.current.srcObject = stream;
                 }
+
+                const vision = await FilesetResolver.forVisionTasks(
+                    'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm'
+                );
+                faceLandmarker = await FaceLandmarker.createFromOptions(
+                    vision,
+                    {
+                        baseOptions: {
+                            modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+                            delegate: 'GPU',
+                        },
+                        outputFaceBlendshapes: true,
+                        outputFacialTransformationMatrixes: true,
+                        runningMode: 'VIDEO',
+                    }
+                );
+
+                webcamRef.current.addEventListener('loadedmetadata', predict);
+
+                setIsLoading(false);
+            } catch (error) {
+                console.error(
+                    'Error setting up video or face landmarker:',
+                    error
+                );
+                setIsLoading(false);
             }
-            requestAnimationFrame(predict);
         };
 
-        setup();
+        setupVideoAndFaceLandmarker();
+
+        return () => {
+            if (webcamRef.current && webcamRef.current.srcObject) {
+                webcamRef.current.srcObject
+                    .getTracks()
+                    .forEach((track) => track.stop());
+            }
+        };
     }, []);
 
-    // TODO: 너구리 선택 후 처리 로직
+    const predict = () => {
+        let startTimeMs = performance.now();
+        if (lastVideoTime !== webcamRef.current.currentTime) {
+            lastVideoTime = webcamRef.current.currentTime;
+            const result = faceLandmarker.detectForVideo(
+                webcamRef.current,
+                startTimeMs
+            );
+            if (
+                result.facialTransformationMatrixes &&
+                result.facialTransformationMatrixes.length > 0 &&
+                result.faceBlendshapes &&
+                result.faceBlendshapes.length > 0
+            ) {
+                const matrix = new Matrix4().fromArray(
+                    result.facialTransformationMatrixes[0].data
+                );
+                rotation = new Euler().setFromRotationMatrix(matrix);
+                blendshapes = result.faceBlendshapes[0].categories;
+            }
+        }
+        requestAnimationFrame(predict);
+    };
+
+    const handleCanPlay = () => {
+        setVideoReady(true);
+        setIsLoading(false);
+        if (videoContainerRef.current) {
+            videoContainerRef.current.style.visibility = 'visible';
+        }
+
+        // 비디오 로드 완료 후 약간의 지연을 주고 페이드 인 효과 적용
+        setTimeout(() => setVideoLoaded(true), 100);
+    };
+
     const handleSelect = () => {
         navigate('/questions');
     };
 
-    //마이크 체크
-    navigator.mediaDevices.enumerateDevices().then((devices) => {
-        const audioDevices = devices.filter(
-            (device) => device.kind === 'audioinput'
-        );
-        console.log(audioDevices);
-    });
-
     useEffect(() => {
-        const audioContext = new (window.AudioContext ||
-            window.webkitAudioContext)();
-        const analyser = audioContext.createAnalyser();
-        analyser.fftSize = 256;
+        const setupMicrophone = async () => {
+            try {
+                const audioContext = new (window.AudioContext ||
+                    window.webkitAudioContext)();
+                const analyser = audioContext.createAnalyser();
+                analyser.fftSize = 256;
 
-        navigator.mediaDevices
-            .getUserMedia({ audio: true })
-            .then((stream) => {
+                const stream = await navigator.mediaDevices.getUserMedia({
+                    audio: true,
+                });
                 const source = audioContext.createMediaStreamSource(stream);
                 source.connect(analyser);
 
@@ -119,23 +135,23 @@ const ChooseRaccoonPage = () => {
                     const height = (average / 255) * 100;
 
                     document.querySelectorAll('.bar').forEach((bar, index) => {
-                        if (index < Math.floor(height / 10)) {
-                            bar.style.height = '70px';
-                            bar.style.backgroundColor = 'orange';
-                        } else {
-                            bar.style.height = '70px';
-                            bar.style.backgroundColor = 'lightgray';
-                        }
+                        bar.style.backgroundColor =
+                            index < Math.floor(height / 10)
+                                ? 'orange'
+                                : 'lightgray';
                     });
 
                     requestAnimationFrame(updateMeter);
                 };
 
                 updateMeter();
-            })
-            .catch((err) => {
-                console.error('Error accessing the microphone', err);
-            });
+                setIsMicReady(true);
+            } catch (err) {
+                console.error('Error accessing the microphone:', err);
+            }
+        };
+
+        setupMicrophone();
     }, []);
 
     return (
@@ -148,7 +164,6 @@ const ChooseRaccoonPage = () => {
                 />
             </header>
             <div className="flex flex-1 flex-col lg:flex-row items-start justify-center gap-10 p-8 overflow-auto">
-                {/* 왼쪽 섹션 */}
                 <div className="w-full lg:w-1/2 flex flex-col items-center justify-start h-full">
                     <h2
                         className="text-6xl sm:text-7xl font-bold mb-8 text-[#8B4513] tracking-wide"
@@ -156,20 +171,24 @@ const ChooseRaccoonPage = () => {
                     >
                         실시간 영상
                     </h2>
-                    <div className="relative w-full h-0 pb-[56.25%] rounded-3xl overflow-hidden shadow-2xl mb-6">
+                    <div
+                        ref={videoContainerRef}
+                        className="relative w-full h-0 pb-[56.25%] rounded-3xl overflow-hidden shadow-2xl mb-6"
+                    >
+                        {isLoading && (
+                            <div className="pulse-container">로딩 중...</div>
+                        )}
                         <video
                             ref={webcamRef}
                             autoPlay
-                            id="video"
-                            className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-500 ${
-                                videoLoaded ? 'opacity-100' : 'opacity-0'
+                            playsInline
+                            onCanPlay={handleCanPlay}
+                            className={`absolute top-0 left-0 w-full h-full object-cover video-fade-in ${
+                                videoLoaded ? 'loaded' : ''
                             }`}
-                        ></video>
-
+                        />
                         <div className="absolute inset-0 bg-gradient-to-t from-black/30 to-transparent"></div>
                     </div>
-
-                    {/* 마이크 입력 테스트 섹션 */}
                     <div className="flex flex-col items-center justify-center w-full">
                         <h2
                             className="text-4xl sm:text-6xl font-bold mb-6 text-[#8B4513] tracking-wide"
@@ -184,13 +203,15 @@ const ChooseRaccoonPage = () => {
                                 <div
                                     key={index}
                                     className="bar w-8 h-32 bg-lightgray rounded-t-lg"
-                                ></div>
+                                    style={{
+                                        height: '70px',
+                                        backgroundColor: 'lightgray',
+                                    }}
+                                />
                             ))}
                         </div>
                     </div>
                 </div>
-
-                {/* 오른쪽 섹션 */}
                 <div className="w-full lg:w-1/2 flex flex-col items-center justify-start h-full">
                     <h2
                         className="text-4xl sm:text-7xl font-bold mb-8 text-[#8B4513] tracking-wide"
@@ -202,7 +223,6 @@ const ChooseRaccoonPage = () => {
                         <ChooseRaccoonHand />
                         <div className="absolute inset-0 bg-gradient-to-t from-black/15 to-transparent rounded-3xl"></div>
                     </div>
-                    {/* 버튼 섹션 */}
                     <div className="flex justify-center space-x-8 mt-8">
                         <button
                             className="bg-gradient-to-r from-[#f7f3e9] to-[#e4d7c7] text-[#8B4513] py-5 px-16 rounded-full border-3 border-[#a16e47] shadow-xl hover:shadow-2xl transition duration-300 ease-in-out transform hover:scale-105 text-xl sm:text-5xl font-bold"
